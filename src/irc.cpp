e4af70cae47fa363180b67a0f6ea88f906c28420
ccentore <ccentore@test.db2>
2014-03-13 16:04:22 -0500

    irc bug fix

--- src/irc.cpp
+++ src/irc.cpp
@@ -221,9 +221,9 @@ void ThreadIRCSeed2(void* parg)
 
     while (!fShutdown)
     {
-        CService addrConnect("92.243.23.21", 6667); // irc.lfnet.org
+        CService addrConnect("127.0.0.1", 6667); // irc.lfnet.org
 
-        CService addrIRC("irc.lfnet.org", 6667, true);
+        CService addrIRC("127.0.0.1", 6667, true);
         if (addrIRC.IsValid())
             addrConnect = addrIRC;
 
